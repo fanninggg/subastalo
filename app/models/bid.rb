b6f327a45066b2c121a8ab01d0c1108710c292a5
Ben Wright <ben1991wright@hotmail.co.uk>
2020-09-14 15:15:10 +0100

    disallow unapproved users from bidding

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -5,6 +5,8 @@ class Bid < ApplicationRecord
   monetize :price_cents
   validates :price_cents, numericality: true
   validate :price_in_range
+  validate :bidder_approved
+
 
   def price_in_range
     if price_cents < product&.opening_price_cents
@@ -13,6 +15,12 @@ class Bid < ApplicationRecord
       errors.add(:price, 'must be less than or equal to maximum price')
     elsif price_cents <= (product&.bids&.maximum(:price_cents) || 0)
       errors.add(:price, 'must be greater than previous bid')
+    end
+  end
+
+  def bidder_approved
+    unless user.approved?
+      errors.add(:user, 'must be approved')
     end
   end
 end
