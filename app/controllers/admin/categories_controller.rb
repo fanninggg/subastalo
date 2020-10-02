b33fa2858481a14135781dd07b64b1f378c23378
Ben Wright <ben1991wright@hotmail.co.uk>
2020-10-02 15:50:25 +0100

    CRUD for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,19 +1,36 @@
 class Admin::CategoriesController < ApplicationController
   before_action :set_categories
   before_action :authenticate_user!
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
   def show
-    @category = Category.find(params[:id])
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
 
   end
 
@@ -24,6 +41,10 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit()
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
