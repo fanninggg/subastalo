124bfbaba1a134a458add8b5a327f38c78180327
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2020-10-21 14:04:21 +0100

    Devise password required removed, styling tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
