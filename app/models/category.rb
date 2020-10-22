eb71226c73ba029adc124d7e35a7ace938448cc3
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2020-10-22 09:56:58 +0100

    Added decorator for truncated name to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   has_many :products, dependent: :destroy
   has_one_attached :photo
   validates :photo, presence: true, blob: { content_type: :image, size_range: 1..100.kilobytes }
+
+  def display_name
+    self.name.truncate(20)
+  end
 end
