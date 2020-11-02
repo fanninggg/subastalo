12a94be9b98fb4c4539d5da2b591374893e10b63
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2020-11-02 16:31:59 +0000

    Changed datetime to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_141201) do
+ActiveRecord::Schema.define(version: 2020_11_02_163007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,7 +148,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_141201) do
     t.string "sex", null: false
     t.boolean "completed", default: false
     t.string "rejection"
-    t.datetime "dob"
+    t.date "dob"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
